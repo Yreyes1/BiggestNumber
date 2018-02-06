17521badadc2036f29f2b2ce8183b534b79b971e
yreyes1 <yreyes1@example.com>
2018-02-06 11:09:41 -0800

    Asks for Numbers and Allows Input

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,14 +1,21 @@
-//Author:
+//Author:Yaneth Reyes Hinojosa
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int number1;
+
+  int number2;
+
   cout<<"Please enter a whole number:\n";
+  cin>>number1;
   
+
   cout<<"Please enter another whole number:\n";
+  cin>>number2;
+
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
